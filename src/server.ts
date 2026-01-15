fed554b5b411e7216cc757956c495a3678629949
James Drake <info@jamesmdrakemedia.com>
2026-01-14 22:18:26 -0500

    Added a simple response to /

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 const PORT = Number(process.env.SERVER_PORT);
 
 app.use(express.json({ limit: "10mb" }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "ping" });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
