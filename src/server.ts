9fce957a99bf274e7bb875fa053e5d6dcb4834be
James Drake <info@jamesmdrakemedia.com>
2026-01-14 22:57:31 -0500

    feat: set up pino logging

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import "dotenv/config";
+import { logger } from "./services/logging";
 
+const log = logger.child({ module: "Server" });
 const app = express();
 const PORT = Number(process.env.SERVER_PORT);
 
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
+  log.trace(`GET /`);
   res.status(200).json({ message: "ping" });
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
+  log.trace(`App started on port ${PORT}`);
 });
---
+++ src/services/logging.ts
@@ -0,0 +1,11 @@
+import { pino, Logger } from "pino";
+
+// export const logger: Logger = pino({
+//   transport: {
+//     target: "pino/file",
+//     options: { destination: "../../app.log" },
+//   },
+//   level: "trace",
+// });
+
+export const logger: Logger = pino({ level: "trace" });
