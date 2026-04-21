ca776f0da1a5886b7c3966e582d84b80b9cf0f68
James Drake <info@jamesmdrakemedia.com>
2026-04-21 19:33:02 -0400

    added logging to rideshare

--- src/jdt-apps-rideshare/controllers/rideshare.ts
+++ src/jdt-apps-rideshare/controllers/rideshare.ts
@@ -2,6 +2,9 @@ import { client } from "../../services/mongo";
 import "dotenv/config";
 import type { Ride } from "../utils/types";
 import type { ObjectId, WithId } from "mongodb";
+import { logger } from "../../services/logging";
+
+const log = logger.child({ module: "Rideshare Controllers" });
 
 const database = client.db("jdt_apps_rideshare");
 const collection = database.collection<Ride>("ride");
@@ -9,6 +12,7 @@ const collection = database.collection<Ride>("ride");
 // TODO: Add JSDoc
 
 export async function getAllRides(): Promise<WithId<Ride>[]> {
+  log.trace("getAllRides()");
   const findResult = collection.find({ deleted_at: null });
 
   const results = [];
@@ -19,12 +23,14 @@ export async function getAllRides(): Promise<WithId<Ride>[]> {
 }
 
 export function getOneRide(id: ObjectId): Promise<WithId<Ride> | null> {
+  log.trace(`getOneRide() id: ${id}`);
   const findResult = collection.findOne({ _id: id, deleted_at: null });
 
   return findResult;
 }
 
 export async function addRide(ride: Ride): Promise<boolean> {
+  log.trace(`addRide() ride.start_time: ${ride.start_time}`);
   const now = new Date().toISOString();
 
   try {
@@ -40,11 +46,13 @@ export async function addRide(ride: Ride): Promise<boolean> {
 
     return result.acknowledged;
   } catch (error) {
+    log.info(`addRide() error: ${error}`);
     return false;
   }
 }
 
 export async function updateRide(ride: Ride): Promise<boolean> {
+  log.trace(`updateRide() ride.start_time: ${ride.start_time}`);
   const now = new Date().toISOString();
   const query = { _id: ride.id };
   const update = {
@@ -64,11 +72,13 @@ export async function updateRide(ride: Ride): Promise<boolean> {
     const result = await collection.updateOne(query, update, options);
     return result.acknowledged;
   } catch (error) {
+    log.info(`updateRide() error: ${error}`);
     return false;
   }
 }
 
 export async function deleteRide(id: ObjectId): Promise<boolean> {
+  log.trace(`deleteRide() id: ${id}`);
   const now = new Date().toISOString();
   const query = { _id: id };
   const update = {
@@ -82,6 +92,7 @@ export async function deleteRide(id: ObjectId): Promise<boolean> {
     const result = await collection.updateOne(query, update, options);
     return result.acknowledged;
   } catch (error) {
+    log.info(`deleteRide() error: ${error}`);
     return false;
   }
 }
--- src/jdt-apps-rideshare/routes/rideshare.ts
+++ src/jdt-apps-rideshare/routes/rideshare.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import * as db from "../controllers/rideshare";
 import { ObjectId } from "mongodb";
+import { logger } from "../../services/logging";
+
+const log = logger.child({ module: "Rideshare Routes" });
 
 export const router = express.Router();
 
 router.get("/rides", async (req, res) => {
+  log.trace("GET /rides");
   res.send(await db.getAllRides());
 });
 
 router.get("/rides/get/:id", async (req, res) => {
+  log.trace("GET /rides/get/:id");
   res.send(await db.getOneRide(ObjectId.createFromHexString(req.params.id)));
 });
 
 router.post("/rides/add", async (req, res) => {
+  log.trace("POST /rides/add");
   if (await db.addRide(req.body)) {
     res.status(200).json({ message: "Success." });
   } else {
@@ -21,6 +27,7 @@ router.post("/rides/add", async (req, res) => {
 });
 
 router.put("/rides/update", async (req, res) => {
+  log.trace("PUT /rides/update");
   if (await db.updateRide(req.body)) {
     res.status(200).json({ message: "Success." });
   } else {
@@ -29,6 +36,7 @@ router.put("/rides/update", async (req, res) => {
 });
 
 router.delete("/rides/delete/:id", async (req, res) => {
+  log.trace("DELETE /rides/delete/:id");
   if (await db.deleteRide(ObjectId.createFromHexString(req.params.id))) {
     res.status(200).json({ message: "Success." });
   } else {
