1863045e029c18445c6e6eac14079e7379d9549b
James Drake <info@jamesmdrakemedia.com>
2026-06-05 16:54:36 -0400

    Temporarily disabling pino file logging

--- src/services/logging.ts
+++ src/services/logging.ts
@@ -1,11 +1,11 @@
 import { pino, Logger } from "pino";
 
-export const logger: Logger = pino({
-  transport: {
-    target: "pino/file",
-    options: { destination: "../../app.log" },
-  },
-  level: "info",
-});
+// export const logger: Logger = pino({
+//   transport: {
+//     target: "pino/file",
+//     options: { destination: "../../app.log" },
+//   },
+//   level: "info",
+// });
 
-// export const logger: Logger = pino({ level: "trace" });
+export const logger: Logger = pino({ level: "info" });
