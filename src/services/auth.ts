cdb0d7ba57692dc64603f930c62434ca3772ec5c
James Drake <info@jamesmdrakemedia.com>
2026-06-29 14:16:46 -0400

    Added SIGNUP_ENABLED flag, changed collection name

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,18 +3,22 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { client } from "./mongo.js";
 import { origins } from "../common/globals.js";
 
+const SIGNUP_ENABLED = process.env.SIGNUP_ENABLED === "1";
+
 const db = client.db();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
   }),
+  collectionPrefix: "btath",
   emailAndPassword: {
     enabled: true,
   },
   session: {
     cookieCache: {
       enabled: false,
+      signupEnabled: SIGNUP_ENABLED,
     },
   },
   trustedOrigins: origins,
