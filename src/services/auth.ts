0dfe7e354b45800ef9f2489365669000da1f61f8
James Drake <info@jamesmdrakemedia.com>
2026-06-29 14:31:47 -0400

    Specified database name for auth

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,15 +3,15 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { client } from "./mongo.js";
 import { origins } from "../common/globals.js";
 
+// TODO: Fix this
 const SIGNUP_ENABLED = process.env.SIGNUP_ENABLED === "1";
 
-const db = client.db();
+const db = client.db("auth");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
   }),
-  collectionPrefix: "btath",
   emailAndPassword: {
     enabled: true,
   },
