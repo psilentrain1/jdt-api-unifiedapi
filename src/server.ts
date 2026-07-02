9b1e8940e0b90984023e2aeed4dac92a6f66ef8c
James Drake <info@jamesmdrakemedia.com>
2026-07-01 20:50:16 -0400

    Initial refactor to use cookies instead of bearer tokens

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,6 @@ app.use(
     credentials: true,
   }),
 );
-app.use(express.json({ limit: "10mb" }));
 
 app.all("/auth/*splat", toNodeHandler(auth));
 
@@ -100,6 +99,12 @@ client
       Sentry.logger.trace(`App started on port ${PORT}`, {
         module: "Server",
       });
+      Sentry.logger.trace(
+        `Auth: Signups enabled? ${process.env.SIGNUP_ENABLED === "1"}`,
+        {
+          module: "Server",
+        },
+      );
     });
   })
   .catch((err) => {
